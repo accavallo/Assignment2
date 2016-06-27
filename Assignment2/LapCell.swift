6c6d3593de25a33e34e465030de4f2c7f7a2c28a
Tony <macuser@macs-macbook.local>
2016-06-27 13:23:14 -0500

    Added everything to the Storyboard. Created a custom lap cell, and model classes.

---
+++ Assignment2/LapCell.swift
@@ -0,0 +1,15 @@
+//
+//  LapCell.swift
+//  Assignment2
+//
+//  Created by Tony on 6/27/16.
+//  Copyright © 2016 Anthony Cavallo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LapCell: UITableViewCell {
+    @IBOutlet var lapNumber: UILabel!
+    @IBOutlet var lapTime: UILabel!
+}
---
+++ Assignment2/Model.swift
@@ -0,0 +1,13 @@
+//
+//  Model.swift
+//  Assignment2
+//
+//  Created by Tony on 6/27/16.
+//  Copyright © 2016 Anthony Cavallo. All rights reserved.
+//
+
+import Foundation
+
+class MyModel: NSObject {
+    
+}
